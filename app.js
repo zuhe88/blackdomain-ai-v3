cc1874eaff1a1dd9b9afd89f4f7799c297484f06
zuhe88 <zhouyang9812@gmail.com>
2026-07-17 00:01:59 +0800

    123

--- app.js
+++ app.js
@@ -4,16 +4,12 @@ const express = require("express");
 const { registerHealthRoutes } = require("./routes/health");
 const { registerImageRoutes } = require("./routes/images");
 const { registerWebhookRoutes } = require("./routes/webhook");
-const { register3AWebhookRoutes } = require("./routes/webhook3a");
-const { registerBoxRoutes } = require("./routes/box");
 
 const app = express();
 
 registerImageRoutes(app);
 registerHealthRoutes(app);
 registerWebhookRoutes(app);
-register3AWebhookRoutes(app);
-registerBoxRoutes(app);
 
 module.exports = {
   app,
