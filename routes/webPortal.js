ab9194ad94bdd28a5e643ed1769c0d76044a067c
zuhe88 <zhouyang9812@gmail.com>
2026-08-13 16:05:25 +0800

    fix: finalize portal result presentation

--- routes/webPortal.js
+++ routes/webPortal.js
@@ -124,7 +124,7 @@ function registerWebPortalRoutes(app) {
         web.cancelReply(replyToken);
         throw error;
       }
-      return res.json({ messages: await pending, portalBuild: "20260813.3" });
+      return res.json({ messages: await pending, portalBuild: "20260813.4" });
     } catch (error) { return next(error); }
   });
 }
