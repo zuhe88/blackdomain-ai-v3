5f35dcc6f048e45075e6a4f0c327179f394ad7d7
zuhe88 <zhouyang9812@gmail.com>
2026-08-13 18:39:15 +0800

    style: add ambient glow to member portal

--- routes/webPortal.js
+++ routes/webPortal.js
@@ -128,7 +128,7 @@ function registerWebPortalRoutes(app) {
         web.cancelReply(replyToken);
         throw error;
       }
-      return res.json({ messages: await pending, portalBuild: "20260813.11" });
+      return res.json({ messages: await pending, portalBuild: "20260813.12" });
     } catch (error) { return next(error); }
   });
 }
